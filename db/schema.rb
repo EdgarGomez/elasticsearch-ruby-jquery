76670ce61511c5339e98047f5f8be4ee2a0d772f
edgar gomez <edgarjgp@gmail.com>
2016-04-08 19:01:50 +0200

    Added avatar columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408165747) do
+ActiveRecord::Schema.define(version: 20160408170053) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "avatar"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
 end
